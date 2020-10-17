b92aa72db9415d224e997c7e5b7796553a8a3d55
Sandeepdora <61022690+Sandeepdora@users.noreply.github.com>
2020-10-17 11:15:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# This file contains code of unit conversion caluculator
+#This code is written in 2 files. They are main.py file and calci.py file.
+#main.py file calls the functions in calci.py file and calci.py contains the caluculations required
+
+'''
+This is code which does conversion of units like length, size, weight, time, currency, etc.,
+
+The bot introduces with a Greeting and asks for the type of conversion. User inputs the type of conversion by giving the number parallel to conversion type.
+
+Then the bot shows the conversation methods in the type and unsers has to select them and give the value which user want to change
+
+And then the bot ends with a good bye message
+
+
+'''
+
+from calci import main
+
+main.main(main)
