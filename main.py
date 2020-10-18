95fc9cb072db7f5aa5429cb3d6673f962e342c23
Sandeepdora <61022690+Sandeepdora@users.noreply.github.com>
2020-10-18 08:55:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,44 @@
 #main.py file calls the functions in calci.py file and calci.py contains the caluculations required
 
 '''
-This is code which does conversion of units like length, size, weight, time, currency, etc.,
+This a bot designed for playing a word game and helping converting units
 
-The bot introduces with a Greeting and asks for the type of conversion. User inputs the type of conversion by giving the number parallel to conversion type.
+This bot greets you with a warm  message. it cheers up you with a cute picture link.
 
-Then the bot shows the conversation methods in the type and unsers has to select them and give the value which user want to change
+Then the bot asks you to select one of the options to do.
 
-And then the bot ends with a good bye message
-
+Then the bot will respond accordingly
 
 '''
 
-from calci import main
+#Importing required files
 
-main.main(main)
+from  game import main as gm
+from  calci import main as cm
+
+
+
+print("Hi there I'm Carlo. I can help ypu cheerup by playing a game or helping in conversion of units. So select one of the option below")
+print("1. Word Game\n2. Unit convertor")
+
+while True:
+    try:
+        choice = int(input())
+    except ValueError:
+        print("Must be a number")
+    else:
+        if choice in [1, 2]:
+            if choice == 1:
+                print("So you are going to Game session. My friend SARA will play the game with you")
+                gm.main(gm)
+            
+            else:
+                print("I'm handing over the part to VICTOR. He will help you in converting units.")
+                cm.main(cm)
+
+
+        else:
+            print("Must be in range 1 and 2")
+
+
+
